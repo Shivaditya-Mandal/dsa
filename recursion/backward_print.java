fb1c7f02548f552ba8dbba3c0b23a384a2b2ebb0
shivaditya <shivadityaisme@gmail.com>
2026-06-26 01:04:08 +0530

    added backward printing

--- recursion/backward_print.java
+++ recursion/backward_print.java
@@ -1,5 +1,16 @@
 package recursion;
 
 public class backward_print {
-    
+    public static void printDec(int n){
+        if(n==1){
+            System.out.print(n);
+            return;
+        }
+        System.out.print(n);
+        printDec(n-1);
+    }
+    public static void main(String args[]){
+        int n = 10 ;
+        printDec(n);
+    }
 }
