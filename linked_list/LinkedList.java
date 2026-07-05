68792617b6dede4b7ac2e899b12488a9e28a6d32
shivaditya <shivadityaisme@gmail.com>
2026-07-05 23:02:41 +0530

    added doubly linked list

--- linked_list/LinkedList.java
+++ linked_list/LinkedList.java
@@ -162,7 +162,7 @@ public class LinkedList{
         return true;
     }
 
-    public static boolean isCycle(){
+    public static boolean isCycle(){ // Floyd's cycle finding algo
         Node slow = head;
         Node fast = head;
         while(fast!=null && fast.next!=null){
@@ -173,6 +173,84 @@ public class LinkedList{
             }
         }
         return false;
+    }
+
+    public static void removeCycle(){
+        Node slow = head;
+        Node fast = head;
+        boolean cycle = false;
+        while(fast != null && fast.next!= null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(fast == slow){
+                cycle = true;
+                break;
+            }
+        }
+        if(cycle==false){
+            return;
+        }
+
+        slow = head;
+        Node prev = null;
+        while (slow != fast){
+            prev = fast ;
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        prev.next = null;
+    }
+
+    private Node getMid(Node head){
+        Node slow = head ;
+        Node fast = head.next;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+
+    private Node merge(Node head1 , Node head2){
+        Node mergedLL = new Node(-1);
+        Node temp = mergedLL;
+
+        while(head1 != null && head2 != null){
+            if(head1.data <= head2.data){
+                temp.next = head1.next;
+                head1=head1.next;
+            }else{
+                temp.next = head2;
+                head2=head2.next;
+                temp = temp.next;
+            }
+        }
+        while(head1 != null){
+            temp.next =  head1;
+            head1=head1.next;
+            temp = temp.next;
+        }
+        while(head2 != null){
+            temp.next =  head2;
+            head2=head2.next;
+            temp = temp.next;
+        }
+        return mergedLL.next;
+
+    }
+    public Node mergeSort(Node head){
+        if(head == null || head.next == null){
+            return head;
+        }
+        Node mid = getMid(head);
+        Node rightHead = mid.next;
+        mid.next = null;
+        Node newLeft = mergeSort(head);
+        Node newRight = mergeSort(rightHead);
+
+        return merge(newLeft,newRight);
     }
     public void print(){
         if(head==null){
@@ -188,7 +266,7 @@ public class LinkedList{
     }
 
     public static void main(String[] args) {
-        // LinkedList ll = new LinkedList();
+        LinkedList ll = new LinkedList();
         // ll.addFirst(1);
         // ll.addFirst(2);
         // ll.addLast(2);
@@ -211,11 +289,23 @@ public class LinkedList{
         // System.out.println("size of the linked list = " + ll.size);
 
 
-        head = new Node(1);
-        head.next = new Node(2);
-        head.next.next = new Node(3);
-        head.next.next.next = head;
-        System.out.println(isCycle());
+        // head = new Node(1);
+        // head.next = new Node(2);
+        // head.next.next = new Node(3);
+        // head.next.next.next = new Node(4);
+        // head.next.next.next.next = head.next;
+        // System.out.println(isCycle());
+        // removeCycle();
+        // System.out.println(isCycle());
+
+        ll.addFirst(1);
+        ll.addFirst(2);
+        ll.addFirst(3);
+        ll.addFirst(4);
+        ll.addFirst(5);
+        ll.print();
+        ll.head = ll.mergeSort(ll.head);
+        ll.print();
     }
 }
 
